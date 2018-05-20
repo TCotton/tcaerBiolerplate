1778d085cff87b5da54a15f227d38b48f09951c0
Andy Walpole <me@andywalpole.me>
2018-05-20 15:31:08 +0100

    added stage-0 babel preset

--- app/index.js
+++ app/index.js
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const objOne = {
+	name: 'Andrew',
+	location: 'London'
+}
+
+const objTwo = {
+	age: 25,
+	...objOne
+}
+
+console.dir(objTwo);
+
 ReactDOM.render(<h1>Boilerplate app</h1>, document.getElementById('root'));
