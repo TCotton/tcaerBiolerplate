4f1ad98e156e596b623abdb0b813d00c0baf5368
Andy Walpole <me@andywalpole.me>
2018-05-23 13:57:42 +0100

    updated boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 	resolve: {
-		alias: {}
+		alias: {
+			Main: path.resolve(path.dirname(''), 'app/components/Main.js'),
+			Nav: path.resolve(path.dirname(''), 'app/components/Nav.js'),
+			Weather: path.resolve(path.dirname(''), 'app/components/Weather.js'),
+			WeatherForm: path.resolve(path.dirname(''), 'app/components/WeatherForm.js'),
+			WeatherMessage: path.resolve(path.dirname(''), 'app/components/WeatherMessage.js'),
+			About: path.resolve(path.dirname(''), 'app/components/About.js'),
+			Examples: path.resolve(path.dirname(''), 'app/components/Examples.js'),
+			openWeatherMap: path.resolve(path.dirname(''), 'app/api/openWeatherMap.js'),
+			ErrorModal: path.resolve(path.dirname(''), 'app/components/errorModel.js'),
+		}
 	},
 	module: {
 		rules: [
@@ -27,6 +39,20 @@ module.exports = {
 				use: [MiniCssExtractPlugin.loader, 'css-loader']
 			},
 			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader' // translates CSS into CommonJS
+					},
+					{
+						loader: 'sass-loader' // compiles Sass to CSS
+					}
+				]
+			},
+			{
 				test: /\.(png|jpg|gif|svg)$/,
 				use: [
 					{
@@ -37,6 +63,7 @@ module.exports = {
 			}
 		]
 	},
+	devtool: 'inline-source-map',
 	plugins: [
 		new HtmlWebPackPlugin({
 			template: './src/index.html',
@@ -44,6 +71,10 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
 			chunkFilename: '[id].css'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
 		})
 	]
 };
